8f70cb9eac015e37c1d0b4cd8ec8e7f495cdc953
satyam singh <satyam884060singh@gmail.com>
2025-04-29 12:00:35 +0530

    re-backend-frontend

--- backend/controllers/book.controllers.js
+++ backend/controllers/book.controllers.js
@@ -14,28 +14,3 @@ export const getBook = async (req,res) => {
     }
 };
 
-export const postBook = async(req,res) => {
-    try{
-        const {name,title,price,category,image} = req.body;
-        
-       
-     
-        const createdBook = new Book({
-            name,
-            title,
-            price,
-            category,
-            image
-           
-        })
-        await createdBook.save();
-        res.status(201).json({message:"Book created successfully",book:{
-         name:createdBook.name,
-         title:createdBook.title,
-         
-        }})
-    } catch(err) {
-        console.log("Error : ",err.message);
-        res.status(500).json({message:"Internal server error"})
-    }
-}
--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -5,6 +5,6 @@ import { getBook, postBook } from "../controllers/book.controllers.js";
 const router = express.Router();
 
 router.get("/",getBook);
-router.post("/add",postBook);
+
 
 export default router;
